fd4e58cd294403d8c57eca528fac9aa42d62e39d
David <ik012108bwd@plaiaundi.net>
2020-09-29 12:19:08 +0200

    Añadiendo cometarios

--- todo/todo.py
+++ todo/todo.py
@@ -5,11 +5,15 @@ from flask import render_template
 from flask import request
 DATABASE = 'db/todo.db'
 
+#Aquí declaramos la aplicación de flask
 app = Flask(__name__)
 
+#Este metodo devuelve una conexión hacia la base de datos
 def getDb():
     conn = sqlite3.connect(DATABASE)
     return conn
+
+#Este metodo carga los resultados de un select en una lista y los devuelve
 def loadTodoTable():
     cur = getDb()
     select = '''SELECT * FROM TODO'''
@@ -20,22 +24,28 @@ def loadTodoTable():
     cur.close()
     return result
 
+#Este metodo nos devuelve la página principal
 @app.route('/')
 def index():    
     return render_template('todo.html')
 
+#Este metodo utiliza el metodo para cargar datos de una tabla y devuelve la página principal pero con los datos cargados
 @app.route('/show')
 def show():
     result=loadTodoTable()
     return render_template('todo.html', value = result)
 
+""" 
+Este metodo recibe valores de un formulario por POST 
+y hace un insert a través de parametros para despues
+devolver la página principal con los datos actualizados
+"""
 @app.route('/insert/',methods=['GET','POST'])
 @app.route('/insert/?<value>',methods=['GET','POST'])
 def insert(value=None):
     cur = getDb()
     fResult = request.form['value']
     if fResult != None:
-        print(fResult + " He llegado ")
         query = ("INSERT INTO TODO(NOMBRE) values(?)")
         cosa = (fResult, )
         cur.execute(query,cosa)
