6193084430147b122beeacbdfd8580993eb66c88
Duvud <ik012108bwd@plaiaundi.net>
2020-10-28 10:44:13 +0100

    10 terminado

--- laravel/septimo/app/Http/Controllers/validarFormulario.php
+++ laravel/septimo/app/Http/Controllers/validarFormulario.php
@@ -9,31 +9,29 @@ use App\Rules\DniCorrecto;
 use App\Rules\EmailCorrecto;
 use App\Rules\CodeRule;
 use Illuminate\Support\Facades\Validator;
+use Redirect;
 
 class validarFormulario extends Controller
 {
     public function validar(Request $req){
-        $sErrores ="";
+        $message=[
+            'required' => 'Tu tio que hay que rellenarlo, no me seas peina calvos .',
+        ];
         //Bloque try catch para manejar todo el tema de la validacion del formulario
         $validacion = Validator::make($req->all(), [
-            //'sexo' => ['in:V,F'],//V o F
-            //'discapacidad' => ['integer','max:100','min:1'],//un valor entre 1 y 100
+            'sexo' => ['in:V,F'],//V o F
+            'discapacidad' => ['integer','max:100','min:1'],//un valor entre 1 y 100
             'fecha_nacimiento' => ['required','date','before:-18 years'],//Que sea mayor de edad
-            //'id' => ['required','regex:/[0-9]{3}[A-Z]{3}/'],//Tres números seguidos de tres letras mayus
-            //'codigo_seguridad' => [new CodeRule],//Si cumple con el ejemplo turbio ese
-            //'dni' => ['required',new DniCorrecto],
-            //'email' => ['required', new EmailCorrecto]
-            
+            'id' => ['required','regex:/[0-9]{3}[A-Z]{3}/'],//Tres números seguidos de tres letras mayus
+            'codigo_seguridad' => [new CodeRule],//Si cumple con el ejemplo turbio ese
+            'dni' => ['required',new DniCorrecto],
+            'email' => ['required', new EmailCorrecto]
         ]);
-        $errors = $validacion->errors();
-        foreach ($errors->all() as $message){
-            $sErrores.=$message;
+        
+        if ($validacion->fails()){
+            return Redirect::back()->withErrors($validacion)->withInput($req->all());
         }
-        if(strcmp($sErrores,"") != 0){
-            return $sErrores;
-        }
-
-            //Si llega hasta aqui es que no ha saltado nada
+        //Si llega hasta aqui es que no ha saltado nada
         return 'test is ok';
     }
 }
--- laravel/septimo/resources/views/form.blade.php
+++ laravel/septimo/resources/views/form.blade.php
@@ -4,21 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{url('/procesarForm')}}" method="POST">
         @csrf
-        <input type="text" placeholder="Nombre" name="nombre">
-        <input type="text" placeholder="Apellido" name="apellido">
-        <input type="text" placeholder="DNI" name="dni">
-        <input type="text" placeholder="Fecha de nacimiento" name="fecha_nacimiento">
-        <input type="text" placeholder="Sexo" name="sexo">
-        <input type="text" placeholder="Direccion" name="direccion">
-        <input type="text" placeholder="Discapacidad" name="discapacidad">
-        <input type="text" placeholder="ID" name="id">
+    <input type="text" placeholder="Nombre" name="nombre" value="{{ old('nombre') }}">
+        <input type="text" placeholder="Apellido" name="apellido" value="{{ old('Apellido') }}">
+        <input type="text" placeholder="DNI" name="dni" value="{{ old('dni') }}">
+        <input type="text" placeholder="Fecha de nacimiento" name="fecha_nacimiento" value="{{ old('fecha_nacimiento') }}">
+        <input type="text" placeholder="Sexo" name="sexo" value="{{ old('sexo') }}">
+        <input type="text" placeholder="Direccion" name="direccion" value="{{ old('direccion') }}">
+        <input type="text" placeholder="Discapacidad" name="discapacidad" value="{{ old('discapacidad') }}">
+        <input type="text" placeholder="ID" name="id" value="{{ old('id') }}">
         <input type="text" placeholder="Codigo de seguridad" name="codigo_seguridad">
-        <input type="text" placeholder="Correo electronico" name="email">
+        <input type="text" placeholder="Correo electronico" name="email" value="{{ old('email') }}">
         <button type="submit">Validar y enviar</button>
     </form>
 </body>
